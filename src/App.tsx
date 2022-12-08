989d1afb9d940a7a4b789552166b1e56d43f9029
longy <longhanzhang999@gmail.com>
2022-12-08 21:53:44 +1030

    TypeScript and react project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import React from 'react'
+
+const App = () => {
+  return (
+    <Container>
+      <h1>this is me</h1>
+    </Container>
+  )
+}
+
+export default App
